2387abe89868ab0f8600debc1474d0b8e8506657
nickboldt <nboldt@redhat.com>
2018-11-16 04:56:59 -0500

    for post-Beta in master, use codeready-workspaces/ and tag 1.0.0
    Change-Id: Iad38510872dbcd168f7a32dff4906ae49de7077e
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- apb/deploy.sh
+++ apb/deploy.sh
@@ -6,8 +6,8 @@ How to use this script:
 -p=,    --project=        | namespace to deploy Code Ready Workspaces
 -c=,    --cert=           | absolute path to a self signed cert OpenShift Console uses
 -oauth, --enable-oauth    | enable Login in with OpenShift
---apb-image=              | installer image, defaults to "registry.access.redhat.com/codeready-workspaces-beta/apb:1.0.0.Beta1". Tag is required
---server-image=           | server image, defaults to "registry.access.redhat.com/codeready-workspaces-beta/server:1.0.0.Beta1". Tag is required
+--apb-image=              | installer image, defaults to "registry.access.redhat.com/codeready-workspaces/apb:1.0.0". Tag is required
+--server-image=           | server image, defaults to "registry.access.redhat.com/codeready-workspaces/server:1.0.0". Tag is required
 -h,     --help            | script help menu
 "
 
@@ -92,16 +92,16 @@ export ENABLE_OPENSHIFT_OAUTH=${ENABLE_OPENSHIFT_OAUTH:-${DEFAULT_ENABLE_OPENSHI
 DEFAULT_CHE_INFRA_KUBERNETES_PVC_STRATEGY="unique"
 export CHE_INFRA_KUBERNETES_PVC_STRATEGY=${CHE_INFRA_KUBERNETES_PVC_STRATEGY:-${DEFAULT_CHE_INFRA_KUBERNETES_PVC_STRATEGY}}
 
-DEFAULT_SERVER_IMAGE_NAME="registry.access.redhat.com/codeready-workspaces-beta/server"
+DEFAULT_SERVER_IMAGE_NAME="registry.access.redhat.com/codeready-workspaces/server"
 export SERVER_IMAGE_NAME=${SERVER_IMAGE_NAME:-${DEFAULT_SERVER_IMAGE_NAME}}
-DEFAULT_SERVER_IMAGE_TAG="1.0.0.Beta1"
+DEFAULT_SERVER_IMAGE_TAG="1.0.0"
 export SERVER_IMAGE_TAG=${SERVER_IMAGE_TAG:-${DEFAULT_SERVER_IMAGE_TAG}}
 
 DEFAULT_APB_NAME="codeready-workspaces"
 export APB_NAME=${APB_NAME:-${DEFAULT_APB_NAME}}
-DEFAULT_APB_IMAGE_NAME="registry.access.redhat.com/codeready-workspaces-beta/apb" # TODO: switch to server-apb?
+DEFAULT_APB_IMAGE_NAME="registry.access.redhat.com/codeready-workspaces/apb" # TODO: switch to server-apb?
 export APB_IMAGE_NAME=${APB_IMAGE_NAME:-${DEFAULT_APB_IMAGE_NAME}}
-DEFAULT_APB_IMAGE_TAG="1.0.0.Beta1"
+DEFAULT_APB_IMAGE_TAG="1.0.0"
 export APB_IMAGE_TAG=${APB_IMAGE_TAG:-${DEFAULT_APB_IMAGE_TAG}}
 
 printInfo() {
