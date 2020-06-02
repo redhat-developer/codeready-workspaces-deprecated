060d550089a46b6d4ac91e75aef1e9fb9b984087
nickboldt <nboldt@redhat.com>
2020-06-02 11:17:47 -0400

    fix path to scripts
    Change-Id: I6f744f0f872d69c29386f16304a9320c05567ef2
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
+
 stacks="golang kamel node10 php python"
 
 if [ "$1" == "clean" ] ; then
-  for b in $stacks ; do
-    rm -Rf $b/target
+  for b in ${stacks} ; do
+    rm -Rf "${b}/target"
   done
   exit 0
 fi
 
-for b in $stacks ; do
-  $b/build.sh
+for b in ${stacks} ; do
+  "${SCRIPT_DIR}/${b}/build.sh"
 done
