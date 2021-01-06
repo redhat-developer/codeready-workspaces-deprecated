08ef558aace40b9ccf60394d64de0949945f5958
nickboldt <nboldt@redhat.com>
2021-01-06 08:10:02 -0500

    support option to run in parallel or series mode; add missing copyright header; don't fail if podman not found on rhel7 nodes
    Change-Id: I3032b751d36867c4c9d838fd67b7a7b3545b10a8
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+
+# Copyright (c) 2018-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 
@@ -11,18 +22,24 @@ if [ "$1" == "clean" ] ; then
   exit 0
 fi
 
-for b in ${stacks} ; do
-  "${SCRIPT_DIR}/${b}/build.sh" &
-done
-wait
+if [ "$2" == "parallel" ] ; then # run builds in parallel, which might consume a lot of memory / disk
+  for b in ${stacks} ; do
+    "${SCRIPT_DIR}/${b}/build.sh" &
+  done
+  wait
+else # run in series, which might take longer but require fewer memory / disk resources
+  for b in ${stacks} ; do
+    "${SCRIPT_DIR}/${b}/build.sh"
+  done
+fi
 
-PODMAN=$(command -v podman)
+PODMAN=$(command -v podman || true)
 if [[ ! -x $PODMAN ]]; then
   echo "[WARNING] podman is not installed."
- PODMAN=$(command -v docker)
+ PODMAN=$(command -v docker || true)
   if [[ ! -x $PODMAN ]]; then
     echo "[ERROR] docker is not installed. Aborting."; exit 1
   fi
 fi
 
-${PODMAN} system prune -af
+${PODMAN} system prune -af || true
