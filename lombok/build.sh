7ee78eb706808e7044eafbaeb9a12cf9a315805f
Nick Boldt <nboldt@redhat.com>
2021-03-10 11:56:40 -0500

    Update build.sh
    simplify - why run a container just to curl a jar?

--- lombok/build.sh
+++ lombok/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-# Copyright (c) 2018-2021 Red Hat, Inc.
+# Copyright (c) 2021 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -11,36 +11,12 @@
 #   Red Hat, Inc. - initial API and implementation
 #
 
-export SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
-
 export LOMBOK_VERSION="1.18.18"
-export IMAGE="registry.access.redhat.com/ubi8"
 
-cd $SCRIPT_DIR
-[[ -e target ]] && rm -Rf target
+mkdir -p target && cd target
+curl -sSLo lombok.jar https://projectlombok.org/downloads/lombok-${LOMBOK_VERSION}.jar
 
-echo ""
-echo "CodeReady Workspaces :: Lombok"
-echo ""
+# now fetch target/lombok.jar in get-sources-jenkins.sh for use in Brew builds
 
-mkdir -p target/lombok
-
-PODMAN=$(command -v podman || true)
-if [[ ! -x $PODMAN ]]; then
-  echo "[WARNING] podman is not installed."
- PODMAN=$(command -v docker || true)
-  if [[ ! -x $PODMAN ]]; then
-    echo "[ERROR] docker is not installed. Aborting."; exit 1
-  fi
-fi
-
-${PODMAN} run --rm -v $SCRIPT_DIR/target/lombok:/lombok ${IMAGE} sh -c "
-    cd /tmp
-    curl -sSL -O https://projectlombok.org/downloads/lombok-${LOMBOK_VERSION}.jar
-    cp lombok-${LOMBOK_VERSION}.jar /lombok
-    "
- 
-tar -czf target/lombok-${LOMBOK_VERSION}-$(uname -m).tar.gz -C target/lombok .
-
-${PODMAN} rmi -f ${IMAGE}
-
+# Or... move this downstream entirely into get-sources-jenkins for the 4 java based plugin sidecars, eg., 
+# https://github.com/redhat-developer/codeready-workspaces-images/blob/crw-2-rhel-8/codeready-workspaces-plugin-java11/get-sources-jenkins.sh#L56-L57
